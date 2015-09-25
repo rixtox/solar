f00bea72ad80af6f95c45a5da5c102b2298e4af9
Rix <i@rix.li>
2015-09-25 03:20:34 -0400

    clear before build

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,11 @@
+import fs from 'fs.extra';
 import webpack from 'webpack';
 
 import webpack_config from './webpack.prod.config';
 import callback from './utils/webpack-callback';
+
+fs.rmrf(webpack_config.output.path);
+fs.mkdirp(webpack_config.output.path);
 
 const compiler = webpack(webpack_config);
 compiler.run(callback);
